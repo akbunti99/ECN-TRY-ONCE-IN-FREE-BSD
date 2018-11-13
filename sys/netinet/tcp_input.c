07a2decdc754b4bc71ac638720f0a6790ceeace2
Abhishek kumar <43776807+akbunti99@users.noreply.github.com>
2018-11-14 01:14:52 +0530

    Update tcp_input.c
    SENDING ECHO FLAG

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -1073,7 +1073,7 @@ findpcb:
 		if ((thflags & (TH_RST|TH_ACK|TH_SYN)) == TH_ACK) {
 
 			INP_INFO_RLOCK_ASSERT(&V_tcbinfo);
-			if(TH_ACK && TH_SYN)
+			if(TH_ACK && TH_SYN && IPTOS_ECN_CE)
 			TH_ECNECHO = 1;     /* for syn|ack */
 			/*
 			 * Parse the TCP options here because
